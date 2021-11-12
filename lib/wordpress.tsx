0d2164922ac9c96252d47cb9ed465494a5da9e83
migCap <migcapdev@gmail.com>
2021-11-12 12:08:41 +0100

    (feature/wp-integration): add url constants and change helper methods

--- lib/wordpress.tsx
+++ lib/wordpress.tsx
@@ -1,40 +1,29 @@
 const BASE_URL = "https://www.rauldediego.es/wp-json/wp/v2";
+const POSTS_API_URL = `${BASE_URL}/posts`;
+const MEDIA_API_URL = `${BASE_URL}/media`;
 
 export async function getPosts() {
-  const postsRes = await fetch(BASE_URL + "/posts");
+  const postsRes = await fetch(POSTS_API_URL);
   const posts = await postsRes.json();
   return posts;
 }
 
 export async function getPost(slug: any) {
   const posts = await getPosts();
-  const postArray = posts.filter((post: any) => post.slug == slug);
+  const postArray = posts.filter((post: any) => post.slug === slug);
   const post = postArray.length > 0 ? postArray[0] : null;
   return post;
-}
-export async function getEvents() {
-  const eventsRes = await fetch(BASE_URL + "/events");
-  const events = await eventsRes.json();
-  return events;
-}
-
-export async function getEvent(slug: any) {
-  const events = await getEvents();
-  const eventArray = events.filter((event: any) => event.slug == slug);
-  const event = eventArray.length > 0 ? eventArray[0] : null;
-  return event;
 }
 
 export async function getSlugs(type: any) {
   let elements = [];
+
   switch (type) {
     case "posts":
       elements = await getPosts();
       break;
-    case "events":
-      elements = await getEvents();
-      break;
   }
+
   const elementsIds = elements.map((element: any) => {
     return {
       params: {
@@ -42,17 +31,25 @@ export async function getSlugs(type: any) {
       },
     };
   });
+
   return elementsIds;
 }
 
 export async function getMedia() {
-  const mediaRes = await fetch(BASE_URL + "/media?per_page=30");
+  // const mediaRes = await fetch(MEDIA_API_URL);
+  const mediaRes = await fetch(MEDIA_API_URL + "?per_page=30");
   const media = await mediaRes.json();
-  console.log(`🚀 ~ getMedia ~ media`, media);
   return media;
 }
 
 export function getFeaturedMedia(media: any, id: any) {
   const featuredMediaArray = media.filter((element: any) => element.id === id);
   return featuredMediaArray.length > 0 ? featuredMediaArray[0] : null;
+}
+
+export async function getFeaturedImage(id: any) {
+  // const mediaRes = await fetch(`${MEDIA_API_URL}/${id}`);
+  // const image = await mediaRes.json();
+  // return image;
+  return id;
 }
--- utils/utils.tsx
+++
@@ -1,7 +0,0 @@
-export function getDate(date: any) {
-  return new Date(date).toLocaleDateString("en-GB", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
-}
