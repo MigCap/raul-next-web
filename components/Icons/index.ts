ca979543b8adc8b8d2ef3d1b0e335bac74238926
migCap <migcapdev@gmail.com>
2022-01-05 09:58:04 +0100

    add a new svg to be used as decoration part

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -2,5 +2,6 @@ import Logo from "components/Icons/Logo";
 import IconLinkedin from "components/Icons/IconLinkedin";
 import IconInstagram from "components/Icons/IconInstagram";
 import Filigrana from "components/Icons/Filigrana";
+import Filigrana2 from "components/Icons/Filigrana2";
 
-export { Logo, IconLinkedin, IconInstagram, Filigrana };
+export { Logo, IconLinkedin, IconInstagram, Filigrana, Filigrana2 };
